fef039749e19775f84b49d9ada66e6dcc883b3d2
AngelMarcelo <ammarce@gmail.com>
2021-12-07 17:32:25 -0300

    se agrega nodemon

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import expres from 'express';
-console.log("Hola Mundo")
+import express from 'express';
+//creo una instancia de express
+const app = express();
+// crear un puerto para el servidor
+app.set('port', process.env.PORT || 4000);
+
+app.listen(app.get('port'),()=>{
+    console.log("Estoy en el puerto "+ app.get('port'));
+})
