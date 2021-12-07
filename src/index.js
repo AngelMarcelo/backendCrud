a26e2d1887bb0062747832a0364425d9fe898a76
AngelMarcelo <ammarce@gmail.com>
2021-12-07 18:36:32 -0300

    se instala cors, morgan y se realizan pruebas en postman

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
 //creo una instancia de express
 const app = express();
 // crear un puerto para el servidor
@@ -6,4 +9,19 @@ app.set('port', process.env.PORT || 4000);
 
 app.listen(app.get('port'),()=>{
     console.log("Estoy en el puerto "+ app.get('port'));
-})
+}) 
+
+//midelwares o configuraciones extras
+app.use(morgan('dev')); //da informacion extra a la terminal
+app.use(cors()); //permite recibir peticiones remotas
+//permite recibir objetos en formato Json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//crear rutas
+app.get('/', (req,res)=>{
+       res.send('hola desde el servidor backend')
+});
+app.delete('/borrardatos', (req,res)=>{
+       res.send('aqui vamos a borrar')
+});
